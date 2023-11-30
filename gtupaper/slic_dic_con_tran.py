d6f302b82bad40f3cdf72db718a6b4e0d09684ba
Rony2518 <113433588+Rony2518@users.noreply.github.com>
2023-11-30 20:34:33 +0530

    Delete gtupaper/slic_dic_con_tran.py

--- gtupaper/slic_dic_con_tran.py
+++
@@ -1,5 +0,0 @@
-data = {
-    'Name':["Gordon","Sylvia","Connor","Carlos","Lloyd"],
-    'country':['Syria','Tristan da Cunha','Northern Mariana Islands','Diego Garcia','South Korea'],
-    'Date':['1/10/2078','10/3/2046','6/26/2028','7/30/2098','3/27/2108','5/10/2109','5/9/2101'],
-}
