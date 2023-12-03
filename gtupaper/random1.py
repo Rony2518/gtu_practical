164660023d17207953185b6ace1f5a401b8f7c5e
Rony2518 <113433588+Rony2518@users.noreply.github.com>
2023-12-03 19:48:17 +0530

    Update random1.py

--- gtupaper/random1.py
+++ gtupaper/random1.py
@@ -1,11 +1,7 @@
 import numpy as np
-rn = np.random.rand(3, 3)
-for i in  rn:
-    for j in i:
-        print(f"{j:.2f}",end=' ')
-    print()
-        
 
-rn = np.random.randn(3, 3)
 np.set_printoptions(precision=2)
+rn = np.random.rand(3, 3)
+print(rn)
+rn = np.random.randn(3, 3)
 print(rn)
