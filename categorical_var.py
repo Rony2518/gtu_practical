fe7c9a93b9a54fadaea003df2c3096cf59dd8408
Rony2518 <113433588+Rony2518@users.noreply.github.com>
2023-12-04 10:51:42 +0530

    Update categorical_var.py

--- categorical_var.py
+++ categorical_var.py
@@ -6,8 +6,7 @@ data = {'Gender': ['Male', 'Female', 'Male', 'Female', 'Trans'],
 df = pd.DataFrame(data)
 
 # Converting columns to categorical type
-df['Gender'] = pd.Categorical(df['Gender'], categories=['Male', 'Female'], ordered=False)
-df['Education'] = pd.Categorical(df['Education'], categories=['High School', 'College', 'Postgraduate'], ordered=True)
+df['Gender'] = pd.Categorical(df['Gender'], categories=['Male', 'Female'], ordered=False)
 
 # Displaying the DataFrame
 print(df)
